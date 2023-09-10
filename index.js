7597d6a0f61ffd1c38cfd14e489b79e36715c347
kobetiakvasyl <kobetiakvasyl@gmail.com>
2023-09-10 15:39:18 -0500

    installing neccesary dependencies, configuring initial project folder structure

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const {ORIGIN = '', PORT = 3000} = process.env;
+
+app.use(helmet());
+app.use(cors({
+  origin: ORIGIN,
+  exposedHeaders: ['Content-Disposition'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
-  console.log('LISTENING FOR PORT: ',);
+  console.log('LISTENING FOR PORT: ', PORT);
 })
---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,5 @@
+const authMiddleware = (req, res, next) => {
+  const authInfo = req.headers['Authentication']
+
+  // if (!Info)
+}
